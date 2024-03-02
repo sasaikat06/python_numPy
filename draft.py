8c5fbe451bc8974f8b175d83e708c2117391e60c
Saikat2300 <saikat2300@gmail.com>
2024-03-02 12:00:13 +0600

    split,search,sort,filter

--- draft.py
+++ draft.py
@@ -1,20 +1,33 @@
+
 import numpy as np
-import string as st
-arr = np.array([[1,2],[3,4]]) 
-arr2 = np.array([[5,6],[7,8]])
-# print(set(arr2))
-print("real concate",np.concatenate((arr,arr2)))
-print("concate:",np.concatenate((arr,arr2),axis=1))
-print("Stack:",np.stack((arr,arr2),axis=1))
-print("hstack",np.hstack((arr,arr2)))
-print("vstack",np.vstack((arr,arr2)))
-print("dstack",np.dstack((arr,arr2)))
+# arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], [13, 14, 15]])
 
-# print(newArr)
-# for x in newArr:
-#     # print(x)
-#     for y in x:
-#         for z in y:
-#             print(z)
+# newarr = np.hsplit(arr,3)
+
+# print(newarr)
 
 
+
+
+# sort() method returns a copy of the array, leaving the original array unchanged
+# print(arr.shape)
+# newarr = arr.reshape(2,-1)
+# print(newarr)
+# print(np.sort(newarr))
+arr = np.array([13,5,6,76])
+
+# check = [True,False,True,False]
+
+fileter_arr = []
+
+for element in arr:
+    if element%2 == 1:
+        fileter_arr.append(True)
+    else:
+        fileter_arr.append(False)
+newarr = arr[fileter_arr]
+newarr1 = arr[sorted(fileter_arr)]
+print(sorted(fileter_arr))
+print(fileter_arr)
+print(sorted(newarr))
+print(newarr1)
